7bbb3eaf646fbe27e85fa4dd0e645e417043e35c
Andy J <ajandt@uwlax.edu>
2020-11-11 13:55:27 -0600

    initial commit working version should do some styling to make it look pretty

---
+++ CapacityCounter/Hubs/InOutHub.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+
+namespace CapacityCounter.Hubs
+{
+    public class InOutHub : Hub
+    {
+        public async Task Signal(string direction, int count)
+        {
+            switch (direction)
+            {
+                case "inc":
+                    count++;
+                    break;
+
+                case "dec":
+                    count--;
+                    break;
+            }
+
+            await Clients.All.SendAsync("broadcast", count).ConfigureAwait(false);
+        }
+    }
+}
